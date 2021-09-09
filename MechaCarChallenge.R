c4095dba7189d06da3c958c1e94b1df5fbfde9d9
AbdullahBera <kucukabdullahbera@gmail.com>
2021-09-08 17:45:12 -0700

    Second Deliverable

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,3 +1,5 @@
+
+# Deliverable One 
 library(dplyr)
 Mechcar_car <- read.csv('MechaCar_mpg.csv', check.names = F, stringsAsFactors = F)
 
@@ -5,3 +7,11 @@ linear_analysis1 <- lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + g
 linear_analysis1
 summary(linear_analysis1)
 
+# Deliverable Two 
+Suspension_coils <- read.csv('Suspension_Coil.csv', check.names = F, stringsAsFactors = F)
+
+total_summary <- Suspension_coils %>% summarize(Mean = mean(PSI), Median = median(PSI),
+                                                Variance = var(PSI), SD = sd(PSI))
+
+lot_summary <- Suspension_coils %>% group_by(Manufacturing_Lot) %>% summarize(Mean = mean(PSI), Median = median(PSI),
+                                                                             Variance = var(PSI), SD = sd(PSI))
